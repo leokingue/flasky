a32a903623064ad6d54ee4f43b7056c31b00e5cf
leokingue <ebangkingue@icloud.com>
2023-08-31 22:40:40 +0200

    update the main/views.py of the Main Blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,16 @@ from ..models import User
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        # ...
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['know']=False
+        else:
+            session['know']=True
+        session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('.index'))
     return render_template('index.html',
             form=form, name=session.get('name'),
