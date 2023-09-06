2da3d5e98f590a11e429a15bc7b879b18d3aec11
leokingue <ebangkingue@icloud.com>
2023-09-06 12:52:44 +0200

    update thr project Flasky

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -6,7 +6,7 @@ from app import create_app, db
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app(current_app)
+        self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
