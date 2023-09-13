3fdb75053fab9c8892caad73ba7fd2db4dc55f7c
leokingue <ebangkingue@icloud.com>
2023-09-14 00:34:33 +0200

    update the project

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,6 +16,7 @@ def login():
         user = User.query.filter_by(email=form.email.data.lower()).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
+            current_user.ping()
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
                 next = url_for('main.index')
